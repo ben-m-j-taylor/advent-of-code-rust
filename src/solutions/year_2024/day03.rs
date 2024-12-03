e52417976f1c85fbefc5aed9672908fdc83aa4ab
ben-m-j-taylor <11777727+ben-m-j-taylor@users.noreply.github.com>
2024-12-03 08:23:47 +0000

    Setting up project for 2024 day 3

---
+++ src/solutions/year_2024/day03.rs
@@ -0,0 +1,12 @@
+use crate::util::parse_file_into_lines;
+
+pub fn solve() -> (i64, i64) {
+    const FILE_PATH: &str = "./input_data/2024/day_02/full-input.txt";
+
+    let lines = parse_file_into_lines(FILE_PATH);
+
+    (
+        0,
+        0,
+    )
+}
